985701aedc3974cb28c11bf9e81aee9919702c78
ShaktimanRai6387 <131231249+ShaktimanRai6387@users.noreply.github.com>
2023-05-05 11:44:04 +0530

    Add project files.

---
+++ DemoDropDown/DropDown.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DropDown.aspx.cs" Inherits="DemoDropDown.DropDown" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem>0</asp:ListItem>
+                <asp:ListItem>1</asp:ListItem>
+                <asp:ListItem>2</asp:ListItem>
+              
+            </asp:DropDownList>
+            <asp:Label ID="Label1" runat="server" Text="Label1">
+             
+            </asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" Style="height: 26px;" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#999999"
+                BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical">
+                <AlternatingRowStyle BackColor="#DCDCDC" />
+                <FooterStyle BackColor="Red" ForeColor="Black" />
+                <HeaderStyle BackColor="000084" Font-Bold="true" ForeColor="White" />
+                <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+                <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#0000A9" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#000065" />
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ DemoDropDown/DropDown.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DemoDropDown
+{
+    public partial class DropDown : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["shakti"].ConnectionString);
+
+            ////2 Open  Connection
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            //// 3 Create a Command
+
+            SqlCommand cmd = new SqlCommand("Select * from UserDetail", con);
+            //DataTable dt = new DataTable();
+            GridView1.DataSource = cmd.ExecuteReader();
+            DropDownList1.DataBind();
+            DropDownList1.DataTextField = "Name";
+            DropDownList1.DataValueField = "ID";
+            DropDownList1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //1 Create Connection
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["shakti"].ConnectionString);
+
+            //2 Open  Connection
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            // 3 Create a Command
+
+           // SqlCommand cmd = new SqlCommand("select * from UserDetail where id = '" + DropDownList1.SelectedValue + "'", con);
+
+            SqlCommand cmd = new SqlCommand("Select * from UserDetail", con);
+            //4 Execute Command
+
+            GridView1.DataSource = cmd.ExecuteReader();
+            GridView1.DataBind();
+            
+            Label1.Text = "record found";
+
+            // 5 Connection Close
+
+            if (con.State == ConnectionState.Open)
+                con.Close();
+
+            //DataTable dt = new DataTable();
+            //cmd.Fill(dt);
+            //DropDownList1.DataSource = dt;
+            //DropDownList1.DataBind();
+            //DropDownList1.DataTextField = "Name";
+            //DropDownList1.DataValueField = "ID";
+            //DropDownList1.DataBind();
+
+        }
+    }
+}
